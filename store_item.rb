3099d9383fccfcbc548e2ae43809139f1808f02d
Francois-Rene <leautier.fr@gmail.com>
2022-10-25 15:12:10 -0400

    init and creates store items

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+computer = { color: "black", price: 1000, brand: "Microsoft", warranty: 2 }
+xbox = { :color => "white", :price => 200, :brand => "Microsoft", :warranty => 1 }
+playstation = { color: "white", price: 200, brand: "Sony", warranty: 1 }
+
+puts "Computers are #{computer[:color]}, sell for #{computer[:price]} dollars, are #{computer[:brand]} branded, and have a #{computer[:warranty]} year warranty."
+puts "Xboxes are #{xbox[:color]}, sell for #{xbox[:price]} dollars, are #{xbox[:brand]} branded, and have a #{xbox[:warranty]} year warranty."
