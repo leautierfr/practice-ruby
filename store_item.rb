68aeb5d4d2ac3f9039f0df09eca5a1b81196c072
Francois-Rene <leautier.fr@gmail.com>
2022-10-26 13:29:15 -0400

    store items as a class

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,33 @@
-computer = { color: "black", price: 1000, brand: "Microsoft", warranty: 2 }
-xbox = { :color => "white", :price => 200, :brand => "Microsoft", :warranty => 1 }
-playstation = { color: "white", price: 200, brand: "Sony", warranty: 1 }
+# computer = { color: "black", price: 1000, brand: "Microsoft", warranty: 2 }
+# xbox = { :color => "white", :price => 200, :brand => "Microsoft", :warranty => 1 }
+# playstation = { color: "white", price: 200, brand: "Sony", warranty: 1 }
 
-puts "Computers are #{computer[:color]}, sell for #{computer[:price]} dollars, are #{computer[:brand]} branded, and have a #{computer[:warranty]} year warranty."
-puts "Xboxes are #{xbox[:color]}, sell for #{xbox[:price]} dollars, are #{xbox[:brand]} branded, and have a #{xbox[:warranty]} year warranty."
+# puts "Computers are #{computer[:color]}, sell for #{computer[:price]} dollars, are #{computer[:brand]} branded, and have a #{computer[:warranty]} year warranty."
+# puts "Xboxes are #{xbox[:color]}, sell for #{xbox[:price]} dollars, are #{xbox[:brand]} branded, and have a #{xbox[:warranty]} year warranty."
+
+class Product
+  attr_reader :name, :color, :price, :brand, :warranty
+  attr_writer :warranty
+
+  def initialize(input)
+    @name = input[:name]
+    @color = input[:color]
+    @price = input[:price]
+    @brand = input[:brand]
+    @warranty = input[:warranty]
+  end
+
+  def print_name
+    puts "#{name} is the product you're looking for."
+  end
+
+  def print_color
+    puts "This product is #{color}."
+  end
+end
+
+product1 = Product.new(name: "Xbox", color: "white", price: 200, brand: "Microsoft", warranty: 1)
+product1.print_name
+product1.print_color
+product1.warranty = 2
+p product1.warranty
